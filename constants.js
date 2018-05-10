5372cee66083ed16b8c57084c1646545c53e92c4
Gregor Moehler <gmoeh@gmx.de>
2018-05-10 22:17:53 +0200

    redo delay to show for now

--- constants.js
+++ constants.js
@@ -1,27 +1,27 @@
 "use strict"
 
 // maximum number of pois that can be configured
-const N_POIS=10;
+exports.N_POIS=10;
 
 // this is int( N_NUM_IMAGE_SECTIONS*4096/(N_PIXELS*3) )
 // max is 255 because of uint8_t
-const N_FRAMES=227;
-const N_PIXELS=60;
+exports.N_FRAMES=227;
+exports.N_PIXELS=60;
 
 // assuming a 2MB flash partition, this is less than
 // 2M / ( 4096 * N_NUM_IMAGE_SECTIONS ) = 49
 // assuming a 1.5MB flash partition, this is less than
 // 1503232 / ( 4096 * N_NUM_IMAGE_SECTIONS ) = 36
-const N_SCENES=36 // 49
+exports.N_SCENES=36 // 49
 
-const N_CMD_FIELDS=6;
+exports.N_CMD_FIELDS=6;
 
 // this is due to max NVS length of 1984 bytes
-const N_PROG_STEPS=330;
+exports.N_PROG_STEPS=330;
 
 // serial baud rate
-//const UART_BAUD=115200
-const UART_BAUD=921600
+//exports.UART_BAUD=115200
+exports.UART_BAUD=921600
 
 // delay after 100 bytes sent
-const pauseAfter100Bytes = 40;
+exports.pauseAfter100Bytes = 200;
